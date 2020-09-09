e582e958e6fdcbe79facd59463ecf26f76fbf7ed
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-09-10 08:41:25 +0900

    tmp: 122: Best Time to Buy and Sell Stock II

---
+++ 122_best_time_to_buy_and_shell_stock_ii/main_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+/*
+	## summary
+	in:要素が価格、インデックスが日の配列
+	out: 最大利益
+	基本的には安い時に買って、高い時に売るが本筋。
+
+	- 配列が昇順の場合は0
+	- 配列の最大値と最小値を求める
+		- indexが max < min の場合はNG
+	- 両端から評価して、l > r ならr-1する
+*/
+func maxProfit(prices []int) int {
+
+	max, min := 0, 0
+	l, r := 0, len(prices)-1
+	for l < r {
+		switch {
+		case prices[l] == prices[r]:
+			l, r = l+1, r-1
+		case prices[l] > prices[r]:
+			max = prices[l]
+			r--
+		case prices[l] < prices[r]:
+			min = prices[r]
+			l++
+		}
+	}
+
+	return max - min
+}
+
+func TestMaxProfit(t *testing.T) {
+	tests := []struct {
+		in  []int
+		out int
+	}{
+		{
+			[]int{7, 1, 5, 3, 6, 4},
+			7,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.in), func(t *testing.T) {
+			got := maxProfit(tt.in)
+			if !reflect.DeepEqual(got, tt.out) {
+				t.Errorf("got: %v, want: %v", got, tt.out)
+			}
+		})
+	}
+}
