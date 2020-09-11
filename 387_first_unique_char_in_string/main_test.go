1429cdb4d3e6fcc6b5ff42e788550fc642f5798b
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-09-12 08:31:48 +0900

    tmp

--- 387_first_unique_char_in_string/main_test.go
+++ 387_first_unique_char_in_string/main_test.go
@@ -17,18 +17,18 @@ func firstUniqChar(s string) int {
 	size := len(s)
 	for i := 0; i < size; i++ {
 		f := false
-		for sindex, r := range s {
-			// 基底値の次index以降を対象
-			if i < sindex {
-				fmt.Println(string(r), sindex, string(s[i]))
-				// 同値ならiterationを抜ける
-				if r == rune(s[i]) {
-					f = true
-					break
-				}
+		fmt.Printf("size: %d, s: %s\n", size, s[i:])
+		// 基底値の次要素以降を対象
+		for sindex, r := range s[i+1:] {
+			fmt.Printf("bindex: %d, val: %s, sindex: %d, val: %s\n", i, string(s[i]), sindex, string(r))
+			// 基底indexのruneと同値ならiterationを抜ける
+			if r == rune(s[i]) {
+				f = true
+				fmt.Println(string(r), sindex, string(s[i]), f)
+				break
 			}
 		}
-		// フラグがfalseの場合（同値の値がない）は、indexを返す
+		// 同値がない場合は、indexを返す
 		if !f {
 			return i
 		}
@@ -48,6 +48,10 @@ func TestMaxProfit(t *testing.T) {
 		{
 			"loveleetcode",
 			2,
+		},
+		{
+			"cc",
+			-1,
 		},
 	}
 	for _, tt := range tests {
