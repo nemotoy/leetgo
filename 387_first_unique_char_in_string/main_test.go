046ea1dcbca03d349e8552727457d9423ab1d81d
nemotoy <nemotoy@users.noreply.github.com>
2020-09-12 09:07:51 +0900

    387: first unique char in a string (#7)
    * 387: first unique char in string
    
    * tmp
    
    * fix: logic to pass all cases
    
    * fix: logic to pass all cases
    
    * fix: logic that iterates from a string removed own index
    
    * fix: logic
    
    * fix: comment

---
+++ 387_first_unique_char_in_string/main_test.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+/*
+	## summary
+	- 1文字ならindex 0 を返す
+	- 先頭から1つずつ取り出して、全件検索
+		- 同値がなければindexを保存
+	- 全て見つからなければ-1を返す
+*/
+func firstUniqChar(s string) int {
+
+	size := len(s)
+	// 1文字なら0を返す
+	if size == 1 {
+		return 0
+	}
+	for i := 0; i < size; i++ {
+		f := false
+		// indexを除いた文字列を作る
+		ss := s[:i] + s[i+1:]
+		for _, r := range ss {
+			// 基底indexのruneと同値ならiterationを抜ける
+			if r == rune(s[i]) {
+				f = true
+				break
+			}
+		}
+		// 同値がない場合は、indexを返す
+		if !f {
+			return i
+		}
+	}
+	return -1
+}
+
+func TestFirstUniqChar(t *testing.T) {
+	tests := []struct {
+		in  string
+		out int
+	}{
+		{
+			"leetcode",
+			0,
+		},
+		{
+			"loveleetcode",
+			2,
+		},
+		{
+			"cc",
+			-1,
+		},
+		{
+			"z",
+			0,
+		},
+		{
+			"aadadaad",
+			-1,
+		},
+		{
+			"dddccdbba",
+			8,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.in), func(t *testing.T) {
+			got := firstUniqChar(tt.in)
+			if !reflect.DeepEqual(got, tt.out) {
+				t.Errorf("got: %v, want: %v", got, tt.out)
+			}
+		})
+	}
+}
