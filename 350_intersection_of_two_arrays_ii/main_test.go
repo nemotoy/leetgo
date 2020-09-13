56b1ef091b6447928f65a71d0d890f21fab3d1fc
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-09-13 23:02:58 +0900

    implement function roughly that pass given cases
    350: Intersection of Two Arrays II

--- 350_intersection_of_two_arrays_ii/main_test.go
+++ 350_intersection_of_two_arrays_ii/main_test.go
@@ -8,9 +8,45 @@ import (
 
 /*
 	## summary
+	2つの配列に含まれる整数を配列にして返す。
+	- 配列の長さを小さい方を元に、iterationする。
 */
 func intersect(nums1 []int, nums2 []int) []int {
-	return []int{}
+
+	n1Size := len(nums1)
+	n2Size := len(nums2)
+	result := []int{}
+
+	if n1Size >= n2Size {
+		for _, num2 := range nums2 {
+			f := false
+			for _, num1 := range nums1 {
+				if num2 == num1 {
+					f = true
+					break
+				}
+			}
+			if f {
+				result = append(result, num2)
+			}
+		}
+	} else {
+		for _, num1 := range nums1 {
+			f := false
+			for _, num2 := range nums2 {
+				if num2 == num1 {
+					f = true
+					break
+				}
+			}
+			if f {
+				result = append(result, num1)
+				fmt.Println(result)
+			}
+		}
+	}
+
+	return result
 }
 
 func TestIntersect(t *testing.T) {
@@ -32,7 +68,7 @@ func TestIntersect(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("%v, %v", tt.in1, tt.in2), func(t *testing.T) {
-			got := intersect(tt.in2, tt.in2)
+			got := intersect(tt.in1, tt.in2)
 			if !reflect.DeepEqual(got, tt.out) {
 				t.Errorf("got: %v, want: %v", got, tt.out)
 			}
