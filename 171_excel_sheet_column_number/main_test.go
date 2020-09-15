339d4912c8674aa89d2213b70ceb3ff4a0cb3b7b
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-09-15 09:22:17 +0900

    implement function roughly that pass given cases
    171: Excel Sheet Column Number

--- 171_excel_sheet_column_number/main_test.go
+++ 171_excel_sheet_column_number/main_test.go
@@ -11,8 +11,45 @@ import (
 	1~26: A ~ Z
 	27~52: AA ~ AZ
 	53~  : BA
+
+	1桁なら1~26
+	2桁で、1桁目が A=1, Z=26, 26*1or26, 2桁目は純粋に番号を加算。
+	AA = 27 (1 * 26 +1)
+	ZY = 701 (26 * 26 + 25 )
 */
 func titleToNumber(s string) int {
+
+	if len(s) == 1 {
+		for _, r := range s {
+			return alphabetToNumber(r)
+		}
+	}
+	nums := make([]int, len(s))
+	for _, r := range s {
+		num := alphabetToNumber(r)
+		nums = append(nums, num)
+	}
+	result := 0
+	for i, n := range nums {
+		if i+1 == len(nums) {
+			result += n
+			break
+		}
+		result += n * lettersLen
+	}
+	return result
+}
+
+const alphabetLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lettersLen = len(alphabetLetter)
+
+// アルファベットを数値に変換する（1~26）
+func alphabetToNumber(r rune) int {
+	for i, a := range alphabetLetter {
+		if r == a {
+			return i + 1
+		}
+	}
 	return 0
 }
 
