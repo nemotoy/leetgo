40f6faa2f562f7756cc5b88626f1ad74a5f8d547
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-09-18 18:31:27 +0900

    chore: add comment

--- list/206_reverse_linked_list_test.go
+++ list/206_reverse_linked_list_test.go
@@ -7,12 +7,24 @@ import (
 
 /*
 	## summary
+
+	- ログ
+	head: 1->2->3->4->5, next: 2->3->4->5
+	head: 2->3->4->5, next: 3->4->5
+	head: 3->4->5, next: 4->5
+	head: 4->5, next: 5
+	head: 5
+	head: 4->5, p: 5
+	head: 3->4, p: 5->4
+	head: 2->3, p: 5->4->3
+	head: 1->2, p: 5->4->3->2
 */
 func reverseList(head *ListNode) *ListNode {
+	// 末尾要素まで探索
 	if head == nil || head.Next == nil {
 		return head
 	}
-	p := reverseList(head.Next)
+	p := reverseList(head.Next) // headを返すまではここで再帰する。
 	head.Next.Next = head
 	head.Next = nil
 	return p
