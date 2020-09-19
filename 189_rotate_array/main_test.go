8a7393036ae3200bf4a4d95ea64aa5012cbbad08
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-09-19 21:12:22 +0900

    feat: implement rotate2 that is faster than rotate

--- 189_rotate_array/main_test.go
+++ 189_rotate_array/main_test.go
@@ -13,7 +13,11 @@ import (
 	例）
 	array = [1,2,3,4], k = 2 の期待値は [3,4,1,2]なので、配列の前からk-1番目まではインデックスが +k され、配列後からk番目まではインデックスが -k される。
 	- l-k > k 場合は通らない。
-	1. l < kまで、最後尾の要素を先頭のインデックスに変更する。
+
+	1. rotate
+	l < kまで、最後尾の要素を先頭のインデックスに変更する。
+
+	2. rotate2
 */
 func rotate(nums []int, k int) {
 	l := len(nums)
@@ -22,8 +26,25 @@ func rotate(nums []int, k int) {
 	}
 	for i := 0; i < k; i++ {
 		for j := l - 1; j > 0; j-- {
-			nums[j], nums[j-1] = nums[j-1], nums[j] // 最後尾の要素を先頭のインデックスに変更する。
+			nums[j], nums[j-1] = nums[j-1], nums[j] // 最後尾の要素を先頭に変更する。
 		}
+	}
+}
+
+func rotate2(nums []int, k int) {
+	l := len(nums)
+	if l <= 1 {
+		return
+	}
+	// int型のスライスを長さlでmakeで初期化する。その際、スライスの各要素はゼロ値。
+	a := make([]int, l)
+	for i := 0; i < l; i++ {
+		j := (i + k) % l // TODO:
+		a[j] = nums[i]
+	}
+	// 元の配列の要素を入れ替える
+	for k := 0; k < len(a); k++ {
+		nums[k] = a[k]
 	}
 }
 
@@ -51,7 +72,7 @@ func TestRotate(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("nums: %v, k: %d", tt.in1, tt.in2), func(t *testing.T) {
-			rotate(tt.in1, tt.in2)
+			rotate2(tt.in1, tt.in2)
 			got := tt.in1
 			if !reflect.DeepEqual(got, tt.out) {
 				t.Errorf("got: %v, want: %v", got, tt.out)
