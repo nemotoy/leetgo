8bd70044becab39cefe7c343d490ff66c02dc8f0
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-09-20 10:29:03 +0900

    fix: logic to pass given cases

--- 70_climbing_stairs/main_test.go
+++ 70_climbing_stairs/main_test.go
@@ -8,9 +8,30 @@ import (
 
 /*
 	## summary
+	階段の上階に登る。一度に1か2階段登れる。取り得る選択肢を返す。
+	- 全て1の場合
+	- 2が含まれる場合
+		- 個数は要素の長さ
+	- 全て2の場合
 */
 func climbStairs(n int) int {
-	return 0
+	if n == 1 {
+		return 1
+	}
+	r := 0
+	r += 1 // 全て1の場合
+	// 全て2の場合
+	if n%2 == 0 {
+		r += 1
+	}
+	l := n - 2 + 1
+	// 2が含まれる場合
+	for n-2 > 0 {
+		r += l
+		n -= 2
+		l--
+	}
+	return r
 }
 
 func TestClimbStairs(t *testing.T) {
@@ -23,6 +44,12 @@ func TestClimbStairs(t *testing.T) {
 		},
 		{
 			3, 3,
+		},
+		{
+			4, 5,
+		},
+		{
+			5, 8,
 		},
 	}
 	for _, tt := range tests {
