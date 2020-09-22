276e764bc052d86eb06d9e69fb53be7696d7829e
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-09-22 15:03:17 +0900

    temporary

--- 278_first_bad_version/main_test.go
+++ 278_first_bad_version/main_test.go
@@ -9,14 +9,40 @@ import (
 /*
 	## summary
 	isBadVersion() APIの呼び出し回数を出来るだけ少なくする。
+	一度trueになったら、それ以降のnはいずれもtrueを返す。という性質を利用するはず。(false -> true -> true) を見つける。
 
 	1. 1からnまでイテレーションし、それぞれAPIコールする
+	2.
+		- まずtrueを探す
+			- trueなら、デクリメント
+			- falseなら、インクリメント中央値を見て、
+		- false -> trueを探す
 */
 func firstBadVersion(n int) int {
 	// nは1からnまでなので、基底を1で開始し、nまで。
 	for i := 1; i <= n; i++ {
 		if isBadVersion(i) {
 			return i
+		}
+	}
+	return 0
+}
+
+func firstBadVersion2(n int) int {
+	m := n / 2
+	if isBadVersion(m) {
+		for m <= 0 {
+			if !isBadVersion(m) {
+				return m + 1
+			}
+			m--
+		}
+	} else {
+		for m <= n {
+			if isBadVersion(m) {
+				return m
+			}
+			m++
 		}
 	}
 	return 0
