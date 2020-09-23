3109a6c47a2501ebda665608698d3b8a42b5c585
nemotoy <nemotoy@users.noreply.github.com>
2020-09-23 19:45:09 +0900

    1. Two Sum (#24)
    * 1. Two Sum
    
    * fix: logic to pass given cases
    
    * fix: logic to pass given cases
    
    * fix: j base
    
    * chore: add summary
    
    * refactor
    
    * feat: implement func that uses hash table
    
    * fix: document
    
    * feat: implement twoSumWithHashTable2()
    
    * docs: add to link

---
+++ 1_two_sum/main_test.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+/*
+	## summary
+	numsの要素を2つ加算し、targetになるインデックス群を返す。
+
+	Sol1. ブルートフォース
+	基底値をi,次基底値をj(i+1)とし、i+j(jはlen(nums)まで検索) = targetを順次比較する。
+
+	Sol2. ハッシュテーブル
+*/
+func twoSum(nums []int, target int) []int {
+	for i, n1 := range nums {
+		for j, n2 := range nums[i+1:] {
+			if target == n1+n2 {
+				return []int{i, j + i + 1}
+			}
+		}
+	}
+	return []int{}
+}
+
+func twoSumWithHashTable(nums []int, target int) []int {
+	m := make(map[int]int, len(nums))
+	// k: element, v: index
+	for i := 0; i < len(nums); i++ {
+		m[nums[i]] = i
+	}
+	for i := 0; i < len(nums); i++ {
+		// target - nums[i]が取得したい要素値
+		complement := target - nums[i]
+		if v, ok := m[complement]; ok {
+			if v != i {
+				return []int{i, v}
+			}
+		}
+	}
+	return []int{}
+}
+
+func twoSumWithHashTable2(nums []int, target int) []int {
+	m := make(map[int]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		complement := target - nums[i]
+		if v, ok := m[complement]; ok {
+			return []int{v, i}
+		}
+		m[nums[i]] = i
+	}
+	return []int{}
+}
+
+func TestTwoSum(t *testing.T) {
+	tests := []struct {
+		in  []int
+		in2 int
+		out []int
+	}{
+		{
+			[]int{2, 7, 11, 15},
+			9,
+			[]int{0, 1},
+		},
+		{
+			[]int{3, 2, 4},
+			6,
+			[]int{1, 2},
+		},
+		{
+			[]int{3, 3},
+			6,
+			[]int{0, 1},
+		},
+		{
+			[]int{3, 2, 3},
+			6,
+			[]int{0, 2},
+		},
+		{
+			[]int{2, 5, 5, 11},
+			10,
+			[]int{1, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("nums:%v target:%d", tt.in, tt.in2), func(t *testing.T) {
+			got := twoSumWithHashTable2(tt.in, tt.in2)
+			if !reflect.DeepEqual(got, tt.out) {
+				t.Errorf("got: %v, want: %v", got, tt.out)
+			}
+		})
+	}
+}
--- links.md
+++ links.md
@@ -4,6 +4,7 @@
 
 - [26. Remove Duplicates from Sorted Array](26_remove_duplicates_from_sorted_array/main_test.go)
 - [189. Rotate Array](189_rotate_array/main_test.go)
+- [1. Two Sum](1_two_sum/main_test.go)
 
 ## Strings
 
