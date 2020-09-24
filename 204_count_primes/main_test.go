2cfc2b4d801bd7c145eee0f227cf8a3c61d7b22f
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-09-25 08:55:25 +0900

    temporary use Sieve of Eratosthenes

--- 204_count_primes/main_test.go
+++ 204_count_primes/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"reflect"
 	"testing"
 )
@@ -34,6 +35,40 @@ func isPrime(n int) bool {
 	return true
 }
 
+/*
+	エラトステネスの篩（Sieve of Eratosthenes）：指定された整数以下の全ての素数を発見するための単純なアルゴリズム
+*/
+func countPrimesWithSoE(n int) int {
+
+	searchList := []int{}
+	for i := 2; i <= n; i++ {
+		if i%2 != 0 {
+			searchList = append(searchList, i)
+		}
+	}
+	fmt.Println(searchList)
+	if len(searchList) == 0 {
+		return 0
+	}
+
+	primeList := []int{searchList[0]}
+
+	fmt.Println(primeList)
+
+	sqrt := math.Sqrt(float64(n))
+	fmt.Println(sqrt)
+	for i, n1 := range searchList {
+		if float64(n1) > sqrt {
+			fmt.Println(primeList, searchList[i:])
+			primeList = append(primeList, searchList[i:]...)
+			break
+		}
+		primeList = append(primeList, n)
+	}
+
+	return len(searchList)
+}
+
 func TestCountPrimes(t *testing.T) {
 	tests := []struct {
 		in  int
@@ -48,7 +83,7 @@ func TestCountPrimes(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("%d", tt.in), func(t *testing.T) {
-			got := countPrimes(tt.in)
+			got := countPrimesWithSoE(tt.in)
 			if !reflect.DeepEqual(got, tt.out) {
 				t.Errorf("got: %v, want: %v", got, tt.out)
 			}
