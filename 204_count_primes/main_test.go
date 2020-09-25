49ea61b8b84ec83fe0e7a29444ce9f35c94735b3
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-09-25 09:29:49 +0900

    temporary

--- 204_count_primes/main_test.go
+++ 204_count_primes/main_test.go
@@ -37,36 +37,37 @@ func isPrime(n int) bool {
 
 /*
 	エラトステネスの篩（Sieve of Eratosthenes）：指定された整数以下の全ての素数を発見するための単純なアルゴリズム
+	1. 2からxまでの整数の昇順リストを作成する（以下、探索リスト）
+	2. 探索リストの先頭の要素を素数リスト（最終的に返すのはこのリストの要素数）に移動する。探索リストからその要素の倍数を削除する
+	3. 2の操作を、探索リストの先頭値がxの平方根に達するまで行う
+	4. 探索リストに残った数を素数リストに移動して処理終了
 */
 func countPrimesWithSoE(n int) int {
 
-	searchList := []int{}
-	for i := 2; i <= n; i++ {
-		if i%2 != 0 {
-			searchList = append(searchList, i)
-		}
+	searchList := make([]int, n-1)
+	for i := 0; i+2 <= n; i++ {
+		searchList[i] = i + 2
 	}
-	fmt.Println(searchList)
-	if len(searchList) == 0 {
-		return 0
-	}
-
-	primeList := []int{searchList[0]}
-
-	fmt.Println(primeList)
-
 	sqrt := math.Sqrt(float64(n))
-	fmt.Println(sqrt)
-	for i, n1 := range searchList {
-		if float64(n1) > sqrt {
-			fmt.Println(primeList, searchList[i:])
-			primeList = append(primeList, searchList[i:]...)
+
+	fmt.Println(searchList)
+	primeList := []int{}
+	for len(searchList) > 0 {
+		fn := searchList[0]
+		if float64(fn) > sqrt {
+			fmt.Println("to sqrt")
 			break
 		}
-		primeList = append(primeList, n)
+		primeList = append(primeList, fn) //todo
+		for i, s := range searchList {
+			if s%fn == 0 {
+				fmt.Println(s, fn)
+				searchList = append(searchList[:i], searchList[i+1:]...)
+				fmt.Println(searchList)
+			}
+		}
 	}
-
-	return len(searchList)
+	return len(primeList)
 }
 
 func TestCountPrimes(t *testing.T) {
@@ -79,6 +80,9 @@ func TestCountPrimes(t *testing.T) {
 		},
 		{
 			2, 0,
+		},
+		{
+			4, 2,
 		},
 	}
 	for _, tt := range tests {
