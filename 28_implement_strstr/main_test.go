f0089ecfba49d93728063b727b08bd89dc81f059
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-09-28 08:42:43 +0900

    feat: initial implementation

--- 28_implement_strstr/main_test.go
+++ 28_implement_strstr/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -9,7 +10,18 @@ import (
 	## summary
 */
 func strStr(haystack string, needle string) int {
-	return 0
+	result := 0
+	for _, r := range needle {
+		n := strings.Count(haystack, string(r))
+		if n <= 0 {
+			result = -1
+			break
+		}
+		if result < n {
+			result = n
+		}
+	}
+	return result
 }
 
 func Test(t *testing.T) {
@@ -26,7 +38,7 @@ func Test(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.in+tt.in2, func(t *testing.T) {
+		t.Run(tt.in+"/"+tt.in2, func(t *testing.T) {
 			got := strStr(tt.in, tt.in2)
 			if !reflect.DeepEqual(got, tt.out) {
 				t.Errorf("got: %v, want: %v", got, tt.out)
