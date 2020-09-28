cbcc44c25f9f76980728cf48f0b8cea633a4444a
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-09-29 08:42:40 +0900

    feat: initial implementation

--- 14_longest_common_prefix/main_test.go
+++ 14_longest_common_prefix/main_test.go
@@ -7,8 +7,25 @@ import (
 
 /*
 	## summary
+	先頭要素を基底にして、他配列要素を走査する。
 */
 func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	base := strs[0]
+	for i := 1; i < len(base); i++ {
+		// 配列の1要素目をprefixとし、先頭から1文字ずつ走査する
+		prefix := base[:i]
+		// 配列の2要素目以降を走査する
+		for _, s := range strs[1:] {
+			// sの先頭からprefixの長さ分の文字列と比較する
+			if s[:len(prefix)] != prefix {
+				// 前回走査したprefixを返す
+				return base[:i-1]
+			}
+		}
+	}
 	return ""
 }
 
