36c5b5625222944d9b85d9261b23213e41fbcf5e
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-10-06 08:12:13 +0900

    fix 136 implementation

--- 136_single_number/main_test.go
+++ 136_single_number/main_test.go
@@ -7,17 +7,26 @@ import (
 
 /*
 	## summary
+	空でないint型の配列から、ユニークな要素を取得する。
 	- 重複した値は3回以上エントリされない
 	- 重複しない値は必ず1つである
+
+	1. Hash table
+	- 配列の全ての要素を探索して、ハッシュに値を追加する。その際、重複があれば削除する。これが利用できるのは、前提条件があるから。
+	- 最終的なハッシュテーブルに残る要素は前提条件より必ず1つになるため、それを返す。
+
+	2. Bit Manipulation
+	- a XOR 0 = a
+	- a XOR a = 0
 */
 func singleNumber(nums []int) int {
 
 	table := make(map[int]int)
-	for _, nums := range nums {
-		if vt, ok := table[nums]; !ok { // 重複なし
-			table[nums] = 1
+	for _, num := range nums {
+		if vt, ok := table[num]; !ok { // 重複なし
+			table[num] = 1
 		} else if vt == 1 { // 重複あり
-			delete(table, nums)
+			delete(table, num)
 		}
 	}
 	// テーブルの要素（長さ）は必ず 1
@@ -25,6 +34,14 @@ func singleNumber(nums []int) int {
 		return v
 	}
 	return 0
+}
+
+func singleNumber2(nums []int) int {
+	a := 0
+	for _, n := range nums {
+		a ^= n
+	}
+	return a
 }
 
 func TestSingleNumber(t *testing.T) {
