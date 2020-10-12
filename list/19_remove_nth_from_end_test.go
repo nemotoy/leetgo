8a104a384e092bebcce4be186f492082c7696b17
nemotoy <nemotoy@users.noreply.github.com>
2020-10-12 21:11:40 +0900

    19. Remove Nth Node From End of List (#38)
    * 19. Remove Nth Node From End of List
    
    * feat: initial implementation
    
    * feat: another sol

---
+++ list/19_remove_nth_from_end_test.go
@@ -0,0 +1,113 @@
+package list
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+/*
+	## summary
+	連結リストの先頭からn要素目を削除する。
+	連結リストの要素数をLとすると、削除する要素は L - n+1 となる。ここでのLはリストの次要素がnilである場合を終端とするまでの長さ。
+*/
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+
+	dummy := &ListNode{}
+	dummy.Next = head
+	l := 0
+	first := head
+
+	for first != nil {
+		l++
+		first = first.Next
+	}
+	l -= n
+	first = dummy
+	for l > 0 {
+		l--
+		first = first.Next
+	}
+	first.Next = first.Next.Next
+	return dummy.Next
+}
+
+func removeNthFromEnd2(head *ListNode, n int) *ListNode {
+
+	dummy := &ListNode{}
+	dummy.Next = head
+	first := dummy
+	second := dummy
+
+	for i := 1; i <= n+1; i++ {
+		first = first.Next
+	}
+
+	for first != nil {
+		first = first.Next
+		second = second.Next
+	}
+	second.Next = second.Next.Next
+	return dummy.Next
+}
+
+func TestRemoveNthFromEnd(t *testing.T) {
+	tests := []struct {
+		in1 *ListNode
+		in2 int
+		out *ListNode
+	}{
+		{
+			&ListNode{
+				1, &ListNode{
+					2, &ListNode{
+						3, &ListNode{
+							4, &ListNode{
+								5, nil,
+							},
+						},
+					},
+				},
+			},
+			2,
+			&ListNode{
+				1, &ListNode{
+					2, &ListNode{
+						3, &ListNode{
+							5, nil,
+						},
+					},
+				},
+			},
+		},
+		{
+			&ListNode{
+				1, nil,
+			},
+			1,
+			nil,
+		},
+		{
+			&ListNode{
+				1, &ListNode{
+					2, nil,
+				},
+			},
+			1,
+			&ListNode{
+				1, nil,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("rm(%d)%s", tt.in2, tt.in1.Visualize()), func(t *testing.T) {
+			got := removeNthFromEnd2(tt.in1, tt.in2)
+			if !reflect.DeepEqual(got, tt.out) {
+				t.Errorf("got: %v, want: %v", got, tt.out)
+				if got != nil && tt.out != nil {
+					t.Logf("details; got: %s, want: %s", got.Visualize(), tt.out.Visualize())
+				}
+			}
+		})
+	}
+}
