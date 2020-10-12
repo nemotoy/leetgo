456940a176cf12c00a7667e49b2787589b3f0c9a
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-10-13 08:56:13 +0900

    chore: add test cases

--- node/tree/101_symmetric_tree_test.go
+++ node/tree/101_symmetric_tree_test.go
@@ -23,7 +23,64 @@ func TestIsSymmetric(t *testing.T) {
 	tests := []struct {
 		in  *TreeNode
 		out bool
-	}{}
+	}{
+		{
+			&TreeNode{
+				Val: 1,
+				Left: &TreeNode{
+					Val: 2,
+					Left: &TreeNode{
+						Val:   3,
+						Left:  nil,
+						Right: nil,
+					},
+					Right: &TreeNode{
+						Val:   4,
+						Left:  nil,
+						Right: nil,
+					},
+				},
+				Right: &TreeNode{
+					Val: 2,
+					Left: &TreeNode{
+						Val:   4,
+						Left:  nil,
+						Right: nil,
+					},
+					Right: &TreeNode{
+						Val:   3,
+						Left:  nil,
+						Right: nil,
+					},
+				},
+			},
+			true,
+		},
+		{
+			&TreeNode{
+				Val: 1,
+				Left: &TreeNode{
+					Val:  2,
+					Left: nil,
+					Right: &TreeNode{
+						Val:   3,
+						Left:  nil,
+						Right: nil,
+					},
+				},
+				Right: &TreeNode{
+					Val:  2,
+					Left: nil,
+					Right: &TreeNode{
+						Val:   3,
+						Left:  nil,
+						Right: nil,
+					},
+				},
+			},
+			false,
+		},
+	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("%v", tt.in), func(t *testing.T) {
 			got := isSymmetric(tt.in)
