1a1416942fa1e26e4b09f6cca1c91cf2f7302ad6
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-05-03 11:45:08 +0900

    905. Sort Array By Parity

--- 905_Sort_Array_By_Parity/main_test.go
+++ 905_Sort_Array_By_Parity/main_test.go
@@ -9,27 +9,47 @@ import (
 /*
 	## summary
 	偶数、奇数の順にソートして返す。各要素は順不同。
+	返す配列の要素数はinputと同数で、int型なので、初期化時にはその要素数がゼロ値で含まれる。
 */
 // nolint: gocritic
 func sortArrayByParity(A []int) []int {
-	result := make([]int, len(A))
+	left, right, l := 0, len(A)-1, len(A)
+	result := make([]int, l)
+
+	for i := 0; i < l; i++ {
+		if A[i]%2 == 0 {
+			result[left] = A[i]
+			left++
+		} else {
+			result[right] = A[i]
+			right--
+		}
+	}
+
 	return result
 }
 
 func TestSortArrayByParity(t *testing.T) {
 	tests := []struct {
 		in  []int
-		out []int
+		out [][]int
 	}{
 		{
 			[]int{3, 1, 2, 4},
-			[]int{2, 4, 3, 1},
+			[][]int{
+				{2, 4, 3, 1},
+				{2, 4, 1, 3},
+			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("%v", tt.in), func(t *testing.T) {
 			got := sortArrayByParity(tt.in)
-			if !reflect.DeepEqual(got, tt.out) {
+			isContained := false
+			for _, out := range tt.out {
+				isContained = reflect.DeepEqual(got, out)
+			}
+			if !isContained {
 				t.Errorf("got: %v, want: %v", got, tt.out)
 			}
 		})
