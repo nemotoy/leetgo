f203e9ec1650003e963dd62271306e17d7a0731f
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-05-18 22:46:10 +0900

    226: recursive

---
+++ node/tree/226_Invert_Binary_Tree.go
@@ -0,0 +1,78 @@
+package tree
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	left := invertTree(root.Left)
+	right := invertTree(root.Right)
+	root.Left = right
+	root.Right = left
+	return root
+}
+
+func TestInvertTree(t *testing.T) {
+	tests := []struct {
+		in  *TreeNode
+		out *TreeNode
+	}{
+		{
+			&TreeNode{
+				Val: 4,
+				Left: &TreeNode{
+					Val: 2,
+					Left: &TreeNode{
+						Val: 1,
+					},
+					Right: &TreeNode{
+						Val: 3,
+					},
+				},
+				Right: &TreeNode{
+					Val: 7,
+					Left: &TreeNode{
+						Val: 6,
+					},
+					Right: &TreeNode{
+						Val: 9,
+					},
+				},
+			},
+			&TreeNode{
+				Val: 4,
+				Left: &TreeNode{
+					Val: 7,
+					Left: &TreeNode{
+						Val: 9,
+					},
+					Right: &TreeNode{
+						Val: 6,
+					},
+				},
+				Right: &TreeNode{
+					Val: 2,
+					Left: &TreeNode{
+						Val: 3,
+					},
+					Right: &TreeNode{
+						Val: 1,
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.in), func(t *testing.T) {
+			got := invertTree(tt.in)
+			if !reflect.DeepEqual(got, tt.out) {
+				t.Errorf("got: %v, want: %v", got, tt.out)
+			}
+		})
+	}
+}
