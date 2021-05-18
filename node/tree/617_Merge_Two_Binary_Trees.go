87f8bb068f9b22318f15aaa861dd7be5ac4cfb7a
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-05-18 22:26:27 +0900

    617: using recursion#2

--- node/tree/617_Merge_Two_Binary_Trees.go
+++ node/tree/617_Merge_Two_Binary_Trees.go
@@ -22,6 +22,20 @@ func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
 	return root1
 }
 
+func mergeTrees2(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+	if root1 == nil {
+		return root2
+	}
+	if root2 == nil {
+		return root1
+	}
+	return &TreeNode{
+		Val:   root1.Val + root2.Val,
+		Left:  mergeTrees(root1.Left, root2.Left),
+		Right: mergeTrees(root1.Right, root2.Right),
+	}
+}
+
 func TestMergeTrees(t *testing.T) {
 	tests := []struct {
 		in1 *TreeNode
