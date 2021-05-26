363e848fb163f4e9a08defdfeb3b1990fcb39648
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-05-26 09:23:50 +0900

    add strings_util package

---
+++ str_util/utils.go
@@ -0,0 +1,12 @@
+package strutil
+
+func GetVowelsNumber(s string) int {
+	ret := 0
+	for _, r := range s {
+		switch r {
+		case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+			ret++
+		}
+	}
+	return ret
+}
