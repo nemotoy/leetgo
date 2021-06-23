12141636eb1f0868bfbc845e6056161b654ff6bb
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-06-24 08:21:04 +0900

    1534.1

--- 1534/main_test.go
+++ 1534/main_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"reflect"
 	"testing"
 )
@@ -15,20 +14,26 @@ import (
 	- |arr[i] - arr[j]| <= a ...
 */
 func countGoodTriplets(arr []int, a int, b int, c int) int {
-	l := len(arr)
-	ret := 0
+	l, ret := len(arr), 0
 	for i := 0; i < l; i++ {
 		for j := i + 1; j < l; j++ {
 			for k := j + 1; k < l; k++ {
-				if (int(math.Abs(float64(arr[i]-arr[j]))) <= a) &&
-					(int(math.Abs(float64(arr[j]-arr[k]))) <= b) &&
-					(int(math.Abs(float64(arr[i]-arr[k]))) <= c) {
+				if abs(arr[i]-arr[j]) <= a &&
+					abs(arr[j]-arr[k]) <= b &&
+					abs(arr[i]-arr[k]) <= c {
 					ret++
 				}
 			}
 		}
 	}
 	return ret
+}
+
+func abs(x int) int {
+	if x < 1 {
+		return -x
+	}
+	return x
 }
 
 func TestCountGoodTriplets(t *testing.T) {
