79931c6d914d27e72637478c10b1169bef669c21
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-07-07 08:18:28 +0900

    1748.0

---
+++ 1748/main_test.go
@@ -0,0 +1,42 @@
+package tree
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+/*
+	## summary
+	Return the sum of all the unique elements of nums.
+*/
+func sumOfUnique(nums []int) int {
+	m := make(map[int]int, len(nums))
+	for _, n := range nums {
+		m[n] += 1
+	}
+	ret := 0
+	for k, v := range m {
+		if v == 1 {
+			ret += k
+		}
+	}
+	return ret
+}
+
+func TestSumOfUnique(t *testing.T) {
+	tests := []struct {
+		in  []int
+		out int
+	}{
+		{[]int{1, 2, 3, 2}, 4}, {[]int{1, 1, 1, 1, 1}, 0}, {[]int{1, 2, 3, 4, 5}, 15},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.in), func(t *testing.T) {
+			got := sumOfUnique(tt.in)
+			if !reflect.DeepEqual(got, tt.out) {
+				t.Errorf("got: %v, want: %v", got, tt.out)
+			}
+		})
+	}
+}
