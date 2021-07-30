0b3a96d7c09d0f322816676c8905e974bac76fc9
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-07-30 22:59:42 +0900

    744.1

--- 744/main_test.go
+++ 744/main_test.go
@@ -25,6 +25,15 @@ func nextGreatestLetter(letters []byte, target byte) byte {
 	}
 }
 
+func nextGreatestLetter2(letters []byte, target byte) byte {
+	for _, letter := range letters {
+		if letter > target {
+			return letter
+		}
+	}
+	return letters[0]
+}
+
 func TestNextGreatestLetter(t *testing.T) {
 	tests := []struct {
 		in  []byte
@@ -38,7 +47,7 @@ func TestNextGreatestLetter(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("%v", 'a'), func(t *testing.T) {
-			got := nextGreatestLetter(tt.in, tt.in2)
+			got := nextGreatestLetter2(tt.in, tt.in2)
 			if !reflect.DeepEqual(got, tt.out) {
 				t.Errorf("got: %v, want: %v", got, tt.out)
 			}
