a266b7edf538793e197350211fe2492b21b1db04
yuma-nemoto <yuma-nemoto@dac.co.jp>
2021-10-29 08:27:59 +0900

    2011.0

---
+++ 2011/main_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+/*
+ - ++X and X++ increments the value of the variable X by 1.
+ - --X and X-- decrements the value of the variable X by 1.
+
+ return the final value of X after performing all the operations.
+*/
+func finalValueAfterOperations(operations []string) int {
+	x := 0
+	for _, op := range operations {
+		if op == "X++" || op == "++X" {
+			x++
+		} else {
+			x--
+		}
+	}
+	return x
+}
+
+func TestFinalValueAfterOperations(t *testing.T) {
+	tests := []struct {
+		in  []string
+		out int
+	}{
+		{[]string{"--X", "X++", "X++"}, 1},
+		{[]string{"++X", "++X", "X++"}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.in), func(t *testing.T) {
+			got := finalValueAfterOperations(tt.in)
+			if !reflect.DeepEqual(got, tt.out) {
+				t.Errorf("got: %v, want: %v", got, tt.out)
+			}
+		})
+	}
+}
