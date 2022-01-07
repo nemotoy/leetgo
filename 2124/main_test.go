16073f407ae88602580cffba46f85b7ec0b51f86
yuma-nemoto <yuma-nemoto@dac.co.jp>
2022-01-07 15:21:46 +0900

    2124.0

---
+++ 2124/main_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"testing"
+)
+
+/*
+	## summary
+	"a"が"b"前に含まれる場合は真
+*/
+func checkString(s string) bool {
+	for i := 0; i < len(s); i++ {
+		if s[i] == 'b' {
+			after := s[i:]
+			for i2 := 0; i2 < len(after); i2++ {
+				if after[i2] == 'a' {
+					return false
+				}
+			}
+		}
+	}
+	return true
+}
+
+func TestCheckString(t *testing.T) {
+	tests := []struct {
+		in  string
+		out bool
+	}{
+		{
+			"aaabbb", true,
+		},
+		{
+			"abab", false,
+		},
+		{
+			"bbb", true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.in, func(t *testing.T) {
+			got := checkString(tt.in)
+			if got != tt.out {
+				t.Errorf("got: %v, want: %v", got, tt.out)
+			}
+		})
+	}
+}
