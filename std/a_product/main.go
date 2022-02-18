f4b46423880867ea4536864cabba06558d355ab7
yuma-nemoto <yuma-nemoto@dac.co.jp>
2022-02-18 20:06:45 +0900

    std/ fix product

--- std/a_product/main.go
+++ std/a_product/main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strconv"
 )
 
 func main() {
-	args := os.Args[1:]
-	aa, _ := strconv.Atoi(args[0])
-	bb, _ := strconv.Atoi(args[1])
-	if aa*bb%2 == 0 {
+	var x, y int
+	fmt.Scan(&x, &y)
+	if (x*y)%2 == 0 {
 		fmt.Println("Even")
 		return
 	}
