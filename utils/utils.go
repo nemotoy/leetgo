7d9f36037e728fe97ebbe1d153a1c45d91ea39b5
yuma-nemoto <yuma-nemoto@dac.co.jp>
2022-02-19 10:45:58 +0900

    add util

---
+++ utils/utils.go
@@ -0,0 +1,79 @@
+package utils
+
+func CountVowels(s string) int {
+	ret := 0
+	for _, r := range s {
+		switch r {
+		case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+			ret++
+		}
+	}
+	return ret
+}
+
+func IsVowel(r byte) bool {
+	switch r {
+	case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+		return true
+	}
+	return false
+}
+
+func IsNumeric(r rune) bool {
+	return r < '0' || r > '9'
+}
+
+func IsPalindrome(s string) bool {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		if s[i] != s[j] {
+			return false
+		}
+	}
+	return true
+}
+
+func ReverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func Max(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
+}
+
+func Min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func Abs(x int) int {
+	if x < 1 {
+		return -x
+	}
+	return x
+}
+
+func Sum(n int) int {
+	sum := 0
+	for n > 0 {
+		sum += n % 10
+		n /= 10
+	}
+	return sum
+}
+
+// Greatest common divisor
+func Gcd(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return Gcd(b, a%b)
+}
